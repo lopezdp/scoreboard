a6a5073a5f40d94f575c40eafdf1c0bdcbd923e8
LopezDP <davidplopez@live.com>
2020-01-08 14:22:12 -0500

    feat: adding game score data row in jumbotron

--- src/components/Scores.js
+++ src/components/Scores.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 //import Image from "react-bootstrap/Image";
 //import { Link } from "react-router-dom";
 import Container from "react-bootstrap/Container";
-//import Jumbotron from "react-bootstrap/Jumbotron";
+import Jumbotron from "react-bootstrap/Jumbotron";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Media from "react-media";
@@ -23,7 +23,9 @@ export default class Scores extends Component {
 	render() {
 		return (
 			<Container className="scores-view" fluid>
-				<Row className="scores-view-game-results"></Row>
+				<Row className="scores-view-game-results">
+					<Jumbotron className="results-view">TEST</Jumbotron>
+				</Row>
 			</Container>
 		);
 	}
