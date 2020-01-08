f447530287fc7deaad01f04e9f004196bb63427c
LopezDP <davidplopez@live.com>
2020-01-08 17:44:08 -0500

    feat: Adding game score elements with image assets.

--- src/components/Scores.js
+++ src/components/Scores.js
@@ -7,6 +7,7 @@ import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Media from "react-media";
 import "./Scores.css";
+import Image from "react-bootstrap/Image";
 //import UiLoadBtn from "./UiLoadBtn";
 //import Tooltip from "react-bootstrap/Tooltip";
 //import OverlayTrigger from "react-bootstrap/OverlayTrigger";
@@ -24,7 +25,44 @@ export default class Scores extends Component {
 		return (
 			<Container className="scores-view" fluid>
 				<Row className="scores-view-game-results">
-					<Jumbotron className="results-view">TEST</Jumbotron>
+					<Jumbotron className="results-view">
+						<Row>
+							<Col xs className="league-rank">
+								<Row>
+									<p>5</p>
+								</Row>
+
+								<Row>
+									<p>4</p>
+								</Row>
+							</Col>
+
+							<Col xs={10} className="team-logo">
+								<Row>
+									<p>5</p>
+								</Row>
+
+								<Row>
+									<p>4</p>
+								</Row>
+							</Col>
+
+							<Col className="team-name"></Col>
+
+							<Col className="team-record"></Col>
+
+							<Col className="game-score"></Col>
+
+							<Col className="match-result"></Col>
+
+							<Col className="game-details-link"></Col>
+						</Row>
+						<Row className="press">
+							<p className="text-muted">
+								Fairbairn (HOU): Game-winning 28-yard FG in OT
+							</p>
+						</Row>
+					</Jumbotron>
 				</Row>
 				<Row className="scores-view-game-results">
 					<Jumbotron className="results-view">TEST</Jumbotron>
