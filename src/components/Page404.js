f0345469c3ad8e0c6b454aca7e4ee52c8e7f783c
LopezDP <davidplopez@live.com>
2020-01-08 13:58:52 -0500

    feat: adding 404 component with styling.

---
+++ src/components/Page404.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Page404.css";
+
+export default () => (
+	<div className="page-404">
+		<h3 className="hh3">
+			Sorry, this page does not exist within the Scoreboard web app just
+			yet...
+		</h3>
+	</div>
+);
