15ec5c65669f19fd50ee38fb8d09ac3f571b395a
LopezDP <davidplopez@live.com>
2020-01-09 00:58:08 -0500

    feat: completed Scores component for all mobile device sizes.

--- src/components/Scores.js
+++ src/components/Scores.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 //import Image from "react-bootstrap/Image";
-//import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 import Container from "react-bootstrap/Container";
 import Jumbotron from "react-bootstrap/Jumbotron";
 import Row from "react-bootstrap/Row";
@@ -23,97 +23,440 @@ export default class Scores extends Component {
 
 	render() {
 		return (
-			<Container className="scores-view" fluid>
-				<Row className="scores-view-game-results">
-					<Jumbotron className="results-view">
-						<Row>
-							<Col className="league-rank">
-								<Row>
-									<p>5</p>
-								</Row>
+			<div>
+				<Media
+					query="(max-width: 567px)"
+					render={() => (
+						<Container className="scores-view" fluid>
+							<Row className="scores-view-game-results">
+								<Jumbotron className="results-view">
+									<Row>
+										<Col className="league-rank">
+											<Row>
+												<p>5</p>
+											</Row>
 
-								<Row>
-									<p>4</p>
-								</Row>
-							</Col>
+											<Row>
+												<p>4</p>
+											</Row>
+										</Col>
 
-							<Col className="team-logo">
-								<Row>
-									<Image
-										alt="Bills"
-										src="/img/Bills.png"
-										className="nfl-icon"
-									/>
-								</Row>
+										<Col className="team-logo">
+											<Row>
+												<Image
+													alt="Bills"
+													src="/img/Bills.png"
+													className="nfl-icon"
+												/>
+											</Row>
 
-								<Row>
-									<Image
-										alt="Texans"
-										src="/img/Texans.png"
-										className="nfl-icon"
-									/>
-								</Row>
-							</Col>
+											<Row>
+												<Image
+													alt="Texans"
+													src="/img/Texans.png"
+													className="nfl-icon"
+												/>
+											</Row>
+										</Col>
 
-							<Col className="team-name">
-								<Row>
-									<p>Bills</p>
-								</Row>
+										<Col className="team-name-xs">
+											<Row>
+												<p>Bills</p>
+											</Row>
 
-								<Row>
-									<p>Texans</p>
-								</Row>
-							</Col>
+											<Row>
+												<p>Texans</p>
+											</Row>
+										</Col>
 
-							<Col className="team-record">
-								<Row>
-									<p className="text-muted w-l">(10-6)</p>
-								</Row>
+										<Col className="team-record">
+											<Row>
+												<p className="text-muted w-l">
+													(10-6)
+												</p>
+											</Row>
 
-								<Row>
-									<p className="text-muted w-l">(10-6)</p>
-								</Row>
-							</Col>
+											<Row>
+												<p className="text-muted w-l">
+													(10-6)
+												</p>
+											</Row>
+										</Col>
 
-							<Col className="game-score">
-								<Row>
-									<p className="text-muted points">19</p>
-								</Row>
+										<Col className="game-score">
+											<Row>
+												<p className="text-muted points">
+													19
+												</p>
+											</Row>
 
-								<Row>
-									<p className="points">22</p>
-								</Row>
-							</Col>
+											<Row>
+												<p className="points">22</p>
+											</Row>
+										</Col>
+									</Row>
+									<Row>
+										<Col className="match-result-sm">
+											<Row>
+												<p className="result">FINAL</p>
+											</Row>
+										</Col>
 
-							<Col className="match-result">
-								<Row>
-									<p className="result">FINAL</p>
-								</Row>
-							</Col>
+										<Col className="game-details-link-sm">
+											<Row>
+												<Link
+													to="/game-detail"
+													className="detail-link-sm"
+													onClick={void 0}
+													onTouchStart={() => ""}
+												>
+													<p>GAME DETAILS</p>
 
-							<Col className="game-details-link">
-								<Row>
-									<p className="detail-link">GAME DETAILS</p>
-								</Row>
-							</Col>
-						</Row>
-						<Row className="press">
-							<p className="text-muted press-release">
-								Fairbairn (HOU): Game-winning 28-yard FG in OT
-							</p>
-						</Row>
-					</Jumbotron>
-				</Row>
-				<Row className="scores-view-game-results">
-					<Jumbotron className="results-view">TEST</Jumbotron>
-				</Row>
-				<Row className="scores-view-game-results">
-					<Jumbotron className="results-view">TEST</Jumbotron>
-				</Row>
-				<Row className="scores-view-game-results">
-					<Jumbotron className="results-view">TEST</Jumbotron>
-				</Row>
-			</Container>
+													<Image
+														alt="Arrow"
+														src="/img/arrowIcon.png"
+														className="arrow-icon-sm"
+													/>
+												</Link>
+											</Row>
+										</Col>
+									</Row>
+									<Row className="press">
+										<p className="text-muted press-release">
+											Fairbairn (HOU): Game-winning
+											28-yard FG in OT
+										</p>
+									</Row>
+								</Jumbotron>
+							</Row>
+						</Container>
+					)}
+				/>
+
+				<Media
+					query="(min-width: 568px) and (max-width: 666px)"
+					render={() => (
+						<Container className="scores-view" fluid>
+							<Row className="scores-view-game-results">
+								<Jumbotron className="results-view">
+									<Row>
+										<Col className="league-rank">
+											<Row>
+												<p>5</p>
+											</Row>
+
+											<Row>
+												<p>4</p>
+											</Row>
+										</Col>
+
+										<Col className="team-logo">
+											<Row>
+												<Image
+													alt="Bills"
+													src="/img/Bills.png"
+													className="nfl-icon"
+												/>
+											</Row>
+
+											<Row>
+												<Image
+													alt="Texans"
+													src="/img/Texans.png"
+													className="nfl-icon"
+												/>
+											</Row>
+										</Col>
+
+										<Col className="team-name-sm">
+											<Row>
+												<p>Bills</p>
+											</Row>
+
+											<Row>
+												<p>Texans</p>
+											</Row>
+										</Col>
+
+										<Col className="team-record">
+											<Row>
+												<p className="text-muted w-l">
+													(10-6)
+												</p>
+											</Row>
+
+											<Row>
+												<p className="text-muted w-l">
+													(10-6)
+												</p>
+											</Row>
+										</Col>
+
+										<Col className="game-score">
+											<Row>
+												<p className="text-muted points">
+													19
+												</p>
+											</Row>
+
+											<Row>
+												<p className="points">22</p>
+											</Row>
+										</Col>
+
+										<Col className="match-result-sm">
+											<Row>
+												<p className="result">FINAL</p>
+											</Row>
+										</Col>
+
+										<Col className="game-details-link-sm">
+											<Row>
+												<Link
+													to="/game-detail"
+													className="detail-link-sm"
+													onClick={void 0}
+													onTouchStart={() => ""}
+												>
+													<p>GAME DETAILS</p>
+
+													<Image
+														alt="Arrow"
+														src="/img/arrowIcon.png"
+														className="arrow-icon-sm"
+													/>
+												</Link>
+											</Row>
+										</Col>
+									</Row>
+									<Row className="press">
+										<p className="text-muted press-release">
+											Fairbairn (HOU): Game-winning
+											28-yard FG in OT
+										</p>
+									</Row>
+								</Jumbotron>
+							</Row>
+						</Container>
+					)}
+				/>
+
+				<Media
+					query="(min-width: 667px) and (max-width: 781px)"
+					render={() => (
+						<Container className="scores-view" fluid>
+							<Row className="scores-view-game-results">
+								<Jumbotron className="results-view">
+									<Row>
+										<Col className="league-rank">
+											<Row>
+												<p>5</p>
+											</Row>
+
+											<Row>
+												<p>4</p>
+											</Row>
+										</Col>
+
+										<Col className="team-logo">
+											<Row>
+												<Image
+													alt="Bills"
+													src="/img/Bills.png"
+													className="nfl-icon"
+												/>
+											</Row>
+
+											<Row>
+												<Image
+													alt="Texans"
+													src="/img/Texans.png"
+													className="nfl-icon"
+												/>
+											</Row>
+										</Col>
+
+										<Col className="team-name-md">
+											<Row>
+												<p>Bills</p>
+											</Row>
+
+											<Row>
+												<p>Texans</p>
+											</Row>
+										</Col>
+
+										<Col className="team-record">
+											<Row>
+												<p className="text-muted w-l">
+													(10-6)
+												</p>
+											</Row>
+
+											<Row>
+												<p className="text-muted w-l">
+													(10-6)
+												</p>
+											</Row>
+										</Col>
+
+										<Col className="game-score">
+											<Row>
+												<p className="text-muted points">
+													19
+												</p>
+											</Row>
+
+											<Row>
+												<p className="points">22</p>
+											</Row>
+										</Col>
+
+										<Col className="match-result">
+											<Row>
+												<p className="result">FINAL</p>
+											</Row>
+										</Col>
+
+										<Col className="game-details-link">
+											<Row>
+												<Link
+													to="/game-detail"
+													className="detail-link"
+													onClick={void 0}
+													onTouchStart={() => ""}
+												>
+													<p>GAME DETAILS</p>
+
+													<Image
+														alt="Arrow"
+														src="/img/arrowIcon.png"
+														className="arrow-icon"
+													/>
+												</Link>
+											</Row>
+										</Col>
+									</Row>
+									<Row className="press">
+										<p className="text-muted press-release">
+											Fairbairn (HOU): Game-winning
+											28-yard FG in OT
+										</p>
+									</Row>
+								</Jumbotron>
+							</Row>
+						</Container>
+					)}
+				/>
+
+				<Media
+					query="(min-width: 782px)"
+					render={() => (
+						<Container className="scores-view" fluid>
+							<Row className="scores-view-game-results">
+								<Jumbotron className="results-view">
+									<Row>
+										<Col className="league-rank">
+											<Row>
+												<p>5</p>
+											</Row>
+
+											<Row>
+												<p>4</p>
+											</Row>
+										</Col>
+
+										<Col className="team-logo">
+											<Row>
+												<Image
+													alt="Bills"
+													src="/img/Bills.png"
+													className="nfl-icon"
+												/>
+											</Row>
+
+											<Row>
+												<Image
+													alt="Texans"
+													src="/img/Texans.png"
+													className="nfl-icon"
+												/>
+											</Row>
+										</Col>
+
+										<Col className="team-name">
+											<Row>
+												<p>Bills</p>
+											</Row>
+
+											<Row>
+												<p>Texans</p>
+											</Row>
+										</Col>
+
+										<Col className="team-record">
+											<Row>
+												<p className="text-muted w-l">
+													(10-6)
+												</p>
+											</Row>
+
+											<Row>
+												<p className="text-muted w-l">
+													(10-6)
+												</p>
+											</Row>
+										</Col>
+
+										<Col className="game-score">
+											<Row>
+												<p className="text-muted points">
+													19
+												</p>
+											</Row>
+
+											<Row>
+												<p className="points">22</p>
+											</Row>
+										</Col>
+
+										<Col className="match-result">
+											<Row>
+												<p className="result">FINAL</p>
+											</Row>
+										</Col>
+
+										<Col className="game-details-link">
+											<Row>
+												<Link
+													to="/game-detail"
+													className="detail-link"
+													onClick={void 0}
+													onTouchStart={() => ""}
+												>
+													<p>GAME DETAILS</p>
+
+													<Image
+														alt="Arrow"
+														src="/img/arrowIcon.png"
+														className="arrow-icon"
+													/>
+												</Link>
+											</Row>
+										</Col>
+									</Row>
+									<Row className="press">
+										<p className="text-muted press-release">
+											Fairbairn (HOU): Game-winning
+											28-yard FG in OT
+										</p>
+									</Row>
+								</Jumbotron>
+							</Row>
+						</Container>
+					)}
+				/>
+			</div>
 		);
 	}
 }
